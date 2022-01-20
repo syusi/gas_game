152be5deb8bd9dea8f972589239451fa7e77c4ff
syusi <sawada.yuhki.15@shizuoka.ac.jp>
2022-01-20 17:06:25 +0000

    PWA content add

---
+++ sw.js
@@ -0,0 +1,28 @@
+var CACHE_NAME = 'gasgame_cache';
+var urlsToCache = [
+  '/assets/',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
