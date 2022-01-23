2479568e1ef2499fd622f2a598e670f601b3de8f
syusi <sawada.yuhki.15@shizuoka.ac.jp>
2022-01-23 12:19:36 +0000

    stage add and cache add

--- sw.js
+++ sw.js
@@ -3,8 +3,22 @@ var host = '/gas_game';
 var urlsToCache = [
   '/assets/BGM/meka_ge_renketu02.mp3',
   '/assets/BGM/natsuyasuminotanken.mp3',
+  '/assets/building_house1.png',
+  '/assets/gasball.png',
+  '/assets/money.png',
+  '/assets/pipe1_straight.png',
+  '/assets/pipe2_curve.png',
+  '/assets/saisei.png',
+  '/assets/sora.png',
+  '/assets/stone.png',
   '/assets/backimage.jpeg',
   '/assets/gas_tank.png',
+  'gasGame.html',
+  'gasGame.js',
+  '/stage/stage1.json',
+  '/stage/stage2.json',
+  '/stage/stage3.json',
+  '/stage/stage4.json',
 ];
 urlsToCache = urlsToCache.map((v,s) => host+v);
 
